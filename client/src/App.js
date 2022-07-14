fc824e0f40f9390ea870f2784dbc10070814ff2e
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-07-14 16:30:35 +0600

    project setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
-
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div>App</div>;
+};
 
-export default App
+export default App;
