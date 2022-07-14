4a6bc44a7544a50b52af413cb4b78a79a0670f23
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-07-14 17:18:04 +0600

    signup component completed

--- server/src/v1/routes/index.js
+++ server/src/v1/routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
+var router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
-});
+router.use('/auth', require('./auth'));
+// router.use('/boards', require('./board'));
+// router.use('/boards/:boardId/sections', require('./section'));
+// router.use('/boards/:boardId/tasks', require('./task'));
 
 module.exports = router;
